dfb48e93e820b51b5c20013b10b43290700fae55
Yeeun Ju <yeju@c6r1s2.42seoul.kr>
2022-04-12 21:19:37 +0900

    초고

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,9 +6,9 @@
 
 
 template <typename T>
-void easyfind(T& a, int& b)
+T::iterator easyfind(T& container, int find)
 {
-
+    return std::find(container.begin(). container.end(), find);
 }
 //T가 int의 컨테이너라고 가정하면 이 함수는 매개변수1에서 매개변수2의 첫번째 발생을 찾아야한다.
 //못찾으면 throw 혹은 오류값 반환
--- ex01/Span.hpp
+++ ex01/Span.hpp
@@ -8,3 +8,5 @@ class Span
 {
     
 }
+
+#endif
