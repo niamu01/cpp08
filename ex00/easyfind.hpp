0dc2209f703a268ab46e0448c16eba7091cb2430
Yeeun Ju <yeju@c6r1s2.42seoul.kr>
2022-04-12 20:40:57 +0900

    first commit

---
+++ ex00/easyfind.hpp
@@ -0,0 +1,23 @@
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+#include <iostream>
+
+
+
+template <typename T>
+void easyfind(T& a, int& b)
+{
+
+}
+//T가 int의 컨테이너라고 가정하면 이 함수는 매개변수1에서 매개변수2의 첫번째 발생을 찾아야한다.
+//못찾으면 throw 혹은 오류값 반환
+//표준 컨테이너는 어떻게 작동하나요?
+
+
+//자체테스트를 구현후 제출하시오
+
+//연관 컨테이너를 처리할 필요가 없습니다.
+
+
+#endif
