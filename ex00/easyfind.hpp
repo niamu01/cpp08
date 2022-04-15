7781706dc24274a15348d6a7adb8d62c829b4d2a
niamu01 <niamu01@naver.com>
2022-04-16 01:27:56 +0900

    aa

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -1,52 +1,21 @@
 #ifndef EASYFIND_HPP
-# define EASYFIND_HPP
+#define EASYFIND_HPP
 
 #include <iostream>
 
 #include <algorithm>
+
 #include <deque>
-#include <iostream>
 #include <list>
-#include <stdexcept>
-#include <string>
 #include <vector>
 
-template <typename C>
-typename C::iterator easyfind(C* container, int value) {
-  typename C::iterator iter = std::find(std::begin(*container),
-                                        std::end(*container),
-                                        value);
-  if (iter == std::end(*container))
-    throw std::runtime_error(std::string("Value ") +
-          std::to_string(value) +
-          std::string(" is not in C"));
-  return iter;
-}
-
-/*
-
-class returnError : std::exception
+template<typename T>
+typename T::iterator easyfind(T & container, int i)
 {
-    virtual const char* what() const throw()
-    {
-        return ("Error!\n");
-    };
-};
-
-template <typename T>
-void easyfind(T& container, int value)
-{
-    return std::find(container.begin(). container.end(), find);
+	typename T::iterator iter = std::find(container.begin(), container.end(), i);
+	if (iter == container.end())
+		throw std::exception();
+	return (iter);
 }
-*/
-//T가 int의 컨테이너라고 가정하면 이 함수는 매개변수1에서 매개변수2의 첫번째 발생을 찾아야한다.
-//못찾으면 throw 혹은 오류값 반환
-//표준 컨테이너는 어떻게 작동하나요?
-
-
-//자체테스트를 구현후 제출하시오
-
-//연관 컨테이너를 처리할 필요가 없습니다.
-
 
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,35 +1,89 @@
 #include "easyfind.hpp"
 
-int main(void) {
-  std::deque<int> d;
-  std::list<int> l;
-  std::vector<int> v;
+int main()
+{
+	try
+	{
+		std::cout << "test with vector container: " << std::endl;
 
-  for (int i = 0 ; i < 10 ; ++i) {
-    d.push_back(i);
-    l.push_back(i);
-    v.push_back(i);
-  }
-  try {
-    std::deque<int>::iterator iter = easyfind(&d, 3);
-    std::cout << "Value " << 3 << " found on Index "
-      << std::distance(std::begin(d), iter) << std::endl;
-  } catch (std::exception& e) {
-    std::cerr << e.what() << std::endl;
-  }
-  try {
-    std::list<int>::iterator iter = easyfind(&l, 6);
-    std::cout << "Value " << 6 << " found on Index "
-      << std::distance(std::begin(l), iter) << std::endl;
-  } catch (std::exception& e) {
-    std::cerr << e.what() << std::endl;
-  }
-  try {
-    std::vector<int>::iterator iter = easyfind(&v, 10);
-    std::cout << "Value " << 10 << " found on Index "
-      << std::distance(std::begin(v), iter) << std::endl;
-  } catch (std::exception& e) {
-    std::cerr << e.what() << std::endl;
-  }
-  return 0;
+		std::vector<int> vector(3, 0); //0 0 0
+
+		vector.push_back(1); //0 0 0 1
+		vector.push_back(2); //0 0 0 1 2
+		vector.push_back(3); //0 0 0 1 2 3
+		vector.push_back(2); //0 0 0 1 2 3 2
+
+		std::vector<int>::iterator iter = easyfind(vector, 2);
+
+		std::cout << *iter << std::endl;
+
+		std::cout << "check next: ";
+		iter++;
+		std::cout << *iter << ", ";
+		iter++;
+		std::cout << *iter << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "No value found" << std::endl;
+	}
+
+	try
+	{
+		std::cout << "test with deque container: " << std::endl;
+
+		std::deque<int> deque(3, 0);
+
+		deque.push_back(1);
+		deque.push_back(2);
+		deque.push_back(3);
+
+		std::deque<int>::iterator iter = easyfind(deque, 2);
+
+		std::cout << *iter << std::endl;
+		
+		//std::cout << deque.size();
+
+		//std::cout << "check next: ";
+		//iter++;
+		//std::cout << "size: " << deque.size() << std::endl;;
+		//std::cout << *iter << ", ";
+		//iter++;
+		//std::cout << "size: " << deque.size() << std::endl;;
+		//std::cout << *iter << std::endl;
+		//iter++;
+		//std::cout << "size: " << deque.size() << std::endl;;
+		//std::cout << *iter << std::endl;
+		//iter++;
+		//std::cout << "size: " << deque.size() << std::endl;;
+		//std::cout << *iter << std::endl;
+		//deque.push_back(8);
+		//std::cout << "size: " << deque.size() << std::endl;;
+		//iter++;
+		//std::cout << *iter << std::endl;
+
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "No value found" << std::endl;
+	}
+
+	try
+	{
+		std::cout << "test with list container: " << std::endl;
+
+		std::list<int> list(3, 0);
+
+		list.push_back(1);
+		list.push_back(2);
+		list.push_back(3);
+
+		std::list<int>::iterator iter = easyfind(list, 2);
+
+		std::cout << *iter << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "No value found" << std::endl;
+	}
 }
