c105ca08d01f0b6ad9cfcc4024c305ce70986e42
niamu01 <niamu01@naver.com>
2022-04-18 21:19:44 +0900

    testcase

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -4,8 +4,22 @@ int main()
 {
 
 	std::cout << "-----TEST 1-----" << std::endl;
-	Span sp = Span(10000)
+	Span span(10000);
 	
+	// Functor
+	FillMultiSet func(0);
+	
+	// Inserting in the multiset x numbers using a functor
+	std::generate_n(std::inserter(span.getMultisetInt(), span.getMultisetInt().begin()), span.getSize(), func);
+
+	std::cout << "Printing first 10 numbers: ";
+	
+	// Using a pair so we can check if we don't go futher than the end of the set when printing first 10 elements
+	for (std::pair<int, std::multiset<int>::iterator> i(0, span.getMultisetInt().begin());
+			i.first < 10 && i.second != span.getMultisetInt().end(); i.first++, i.second++)
+		std::cout << *i.second << " ";
+		
+	std::cout << "\nLongest: " << span.longestSpan() << ", shortest: " << span.shortestSpan() << "\n";
 
 	std::cout << "-----SUBJECT TESTCASE-----";
 
